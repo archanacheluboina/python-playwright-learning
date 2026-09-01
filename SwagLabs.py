835c1f646e3db86426e15c250b2fc5e2c794bcd8
Archana Cheluboina <archana.cheluboina@gmail.com>
2026-09-01 16:14:56 +0530

    Add SauceDemo Add-to-cart Playwright test

--- SwagLabs.py
+++ SwagLabs.py
@@ -9,9 +9,24 @@ def test_saucedemo_login(page: Page):
 	page.get_by_role("button", name="Login").click()
 	page.wait_for_timeout(3000)
 
-	expect(page.get_by_text("Sauce Labs Bike Light")).to_be_visible()
+	# Verify successfull login
 	expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+
+	# Adding Bike to cart
+	page.get_by_text("Sauce Labs Bike Light").click()
+	expect(page).to_have_url("https://www.saucedemo.com/inventory-item.html?id=0")
+	expect(page.get_by_text("Sauce Labs Bike Light")).to_be_visible()
+	page.get_by_role("button", name="Add to cart").click()
+	page.locator(".shopping_cart_link").click()
+
+	#check the url of shoping cart page
+	expect(page).to_have_url("https://www.saucedemo.com/cart.html")
+
+	# Verify item is added to cart
+	expect(page.get_by_role("link", name="Sauce Labs Bike Light")).to_be_visible()
+
 	print("Assertion Tested and Locators by placeholder and role are working fine")
+	print("Test Passed - Bike Light successfully added to cart")
 	page.wait_for_timeout(3000)
 
 if __name__ == "__main__":
